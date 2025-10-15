03816a00e0c8923f772f7803de540d8bac2fea73
Anujka <anujinbolormaa308@gmail.com>
2025-10-15 20:09:15 +0800

    Add files via upload

---
+++ shalglt.js
@@ -0,0 +1,36 @@
+/* 2r heseg 14r bodlogo
+let nums = [1, 2, 3, 4, 5, 6, 7];
+
+      let odds = nums.filter((n) => n % 2 !== 0);
+      console.log(odds); */
+
+/*  3r hesgiin 15r bodlogo
+       function countVowels(word) {
+  let count = 0;
+  let vowels = "Hello";
+
+  for (let.countVowels) {
+    if (vowels.includes(count)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+let x = countVowels("hello");
+console.log(x); 
+) */
+
+/* function removeDuplicates(numbers){
+let num [1, 2, 2, 3, 4, 4, 5];
+}
+
+
+console.log(removeDuplicates) */
+
+function findmax(too) {
+    let too = [4, 7, 2, 8, 10, 3];
+}
+console.log(findmax)
+ mdh um algaa
